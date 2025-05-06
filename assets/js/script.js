c2814ba43ee3b475cf687f7ff8fedad498e444e6
brotherally <allyssonmateus123@gmail.com>
2025-05-06 01:51:32 -0300

    Site do Not Today Grinch

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+let currentImageIndex = 0;
+const images = [
+  "assets/images/01.png",
+  "assets/images/02.png",
+  "assets/images/03.png",
+  "assets/images/04.png",
+  "assets/images/05.png",
+  "assets/images/06.png",
+  "assets/images/07.png"
+];
+
+function openLightbox(index) {
+  currentImageIndex = index;
+  document.getElementById("lightbox").style.display = "block";
+  document.getElementById("lightbox-img").src = images[index];
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
+
+function changeImage(direction) {
+  currentImageIndex += direction;
+  if (currentImageIndex < 0) currentImageIndex = images.length - 1;
+  if (currentImageIndex >= images.length) currentImageIndex = 0;
+  document.getElementById("lightbox-img").src = images[currentImageIndex];
+}
